5320790a096d51c6dcd66e9212d5d3aee7654b79
kasatkin <kasatkin87@gmail.com>
2019-04-03 12:49:08 +0000

    Pattern Visitor finished

--- visitor.php
+++ visitor.php
@@ -2,10 +2,29 @@
 
 abstract class Unit
 {
+    protected $depth = 0;
+    
     public function getComposite()
     {
         return null;
     }
+    
+    public function accept(ArmyVisitor $visitor)
+    {
+        $method = "visit" . get_class($this);
+        $visitor->$method($this);
+    }
+    
+    public function getDepth()
+    {
+        return $this->depth;
+    }
+    
+    public function setDepth($depth)
+    {
+        $this->depth = $depth;
+    }
+    
     abstract function getStrength();
 }
 
@@ -33,10 +52,24 @@ abstract class CompositeUnit extends Unit
     
     public function addUnit(Unit $unit)
     {
-        if (in_array($unit, $this->units, true)){
-            return;
+        foreach ($this->units as $thisunit)
+        {
+            if ($unit == $thisunit){
+                return;
+            }    
         }
+        $unit->setDepth($this->depth + 1);
         $this->units[] = $unit;
+    }
+    
+    public function accept(ArmyVisitor $visitor)
+    {
+        $method = "visit" . get_class($this);
+        $visitor->$method($this);
+        foreach ($this->units as $thisunit)
+        {
+            $thisunit->accept($visitor);
+        }
     }
 }
 
@@ -97,18 +130,73 @@ class Army extends CompositeUnit
     }
 }
 
+abstract class ArmyVisitor
+{
+    abstract function visit(Unit $node);
+    
+    public function visitArcher(Archer $node)
+    {
+        $this->visit($node);
+    }
+    
+    public function visitPikeman(Pikeman $node)
+    {
+        $this->visit($node);
+    }
+    
+    public function visitSlinger(Slinger $node)
+    {
+        $this->visit($node);
+    }
+    
+    public function visitArmy(Army $node)
+    {
+        $this->visit($node);
+    }
+}
+
+class TextDumpArmyVisitor extends ArmyVisitor
+{
+    private $text = "";
+    
+    public function visit(Unit $node)
+    {
+        $txt = "";
+        $pad = 4 * $node->getDepth();
+        $txt .= sprintf("%{$pad}s" , "");
+        $txt .= get_class($node) . ": ";
+        $txt .= "Fire Power: " . $node->getStrength() . "\n";
+        $this->text  .= $txt;
+    }
+    
+    public function getText()
+    {
+        return $this->text;
+    }
+}
+
 $main_army = new Army();
 $main_army->addUnit(new Archer());
 $main_army->addUnit(new Pikeman());
 $main_army->addUnit(new Pikeman());
 $main_army->addUnit(new Slinger());
 
+$textDump = new TextDumpArmyVisitor();
+$main_army->accept($textDump);
+print $textDump->getText();
+
 $sub_army = new Army();
-$sub_army->addUnit(new Archer());
-$sub_army->addUnit(new Archer());
+$sub_army->addUnit(new Pikeman());
+$sub_army->addUnit(new Pikeman());
+$sub_army->addUnit(new Slinger());
 
-print_r($main_army->getStrength().PHP_EOL);
-print_r($sub_army->getStrength().PHP_EOL);
+$textDump2 = new TextDumpArmyVisitor();
+$sub_army->accept($textDump2);
+print $textDump2->getText();
 
-var_dump($main_army);
-var_dump($sub_army);
+$general_army = new Army();
+$general_army->addUnit($main_army);
+$general_army->addUnit($sub_army);
+$textDump3 = new TextDumpArmyVisitor();
+$general_army->accept($textDump3);
+print $textDump3->getText();
